2ee911db383ac51a7f3afaaa72cf567b12a9b096
Masaki Ikegami <salvage0707@gmail.com>
2019-05-27 22:41:11 +0900

    mutation action のテンプレートを作成

---
+++ src/store/actions.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import { Auth, List, Task } from '../api'
+/* eslint-enable no-unused-vars */
+
+export default {
+  login: ({ commit }) => {
+    // TODO:
+    throw new Error('login mutation should be implemented')
+  },
+
+  fetchLists: ({ commit }) => {
+    // TODO:
+    throw new Error('fetchLists mutation should be implemented')
+  },
+
+  addTask: ({ commit }) => {
+    // TODO:
+    throw new Error('addTask mutation should be implemented')
+  },
+
+  updateTask: ({ commit }) => {
+    // TODO:
+    throw new Error('updateTask mutation should be implemented')
+  },
+
+  removeTask: ({ commit }) => {
+    // TODO:
+    throw new Error('removeTask mutation should be implemented')
+  },
+
+  logout: ({ commit }) => {
+    // TODO:
+    throw new Error('logout mutation should be implemented')
+  }
+}
